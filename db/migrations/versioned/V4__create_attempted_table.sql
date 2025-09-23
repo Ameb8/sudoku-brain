edea2622f51ddb4ecc084c6d705bfb67122313ee
Alex Buckley <alexmeb81@outlook.com>
2025-09-22 23:50:17 -0700

    Created generic puzzle attempt db table

--- db/migrations/versioned/V4__create_attempted_table.sql
+++ db/migrations/versioned/V4__create_attempted_table.sql
@@ -7,6 +7,6 @@ CREATE TABLE `attempted` (
   `started_on` date NOT NULL,
   PRIMARY KEY (`user_id`,`puzzle_id`),
   KEY `attempted-puzzle_idx` (`puzzle_id`),
-  CONSTRAINT `attempted-puzzle` FOREIGN KEY (`puzzle_id`) REFERENCES `puzzle` (`puzzle_id`),
-  CONSTRAINT `attempted-user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `attempted__puzzle` FOREIGN KEY (`puzzle_id`) REFERENCES `puzzle` (`puzzle_id`),
+  CONSTRAINT `attempted__user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ db/migrations/versioned/V7__create_puzzle_attempt_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE puzzle_attempt (
+    id INT AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    puzzle_id INT UNSIGNED NOT NULL,
+    current_state CHAR(81) NOT NULL,
+    seconds_worked_on INT UNSIGNED NOT NULL,
+    hints_used INT UNSIGNED NOT NULL,
+    started_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    solved_on DATETIME,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT puzzle_attempt__users
+        FOREIGN KEY (user_id) 
+        REFERENCES users(user_id) 
+        ON DELETE CASCADE,
+
+    CONSTRAINT puzzle_attempt__puzzle
+        FOREIGN KEY (puzzle_id)
+        REFERENCES puzzle(puzzle_id)
+        ON DELETE CASCADE,
+
+    INDEX users_puzzle (user_id, puzzle_id),
+    INDEX puzzle_users (puzzle_id, user_id)
+);
